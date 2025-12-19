3ebbd62ed65c33e74979d8f70d30bf09b2cc0574
princeajuzie7 <princeajuzie1@gmail.com>
2025-12-19 16:12:43 +0100

    fix: missing line on icon

--- components/icon.tsx
+++ components/icon.tsx
@@ -140,7 +140,7 @@ export const Google = (props: SVGProps<SVGSVGElement>)=>(
                   <path d="M12 5.38c1.62 0 3.06.56 4.21 1.64l3.15-3.15C17.45 2.09 14.97 1 12 1 7.7 1 3.99 3.47 2.18 7.07l3.66 2.84c.87-2.6 3.3-4.53 6.16-4.53z" fill="#EA4335"/>
          </svg>
 
-
+)
 export const TypeScript = (props: SVGProps<SVGSVGElement>) => (
   <svg {...props} viewBox="0 0 256 256" preserveAspectRatio="xMidYMid">
     <path
