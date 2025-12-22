1df8112a540d6e15d6915900fdf0e2ad2e4744f3
Emmanuel Odii <emmanuelodii80@gmail.com>
2025-12-22 15:51:41 +0100

    feat: core adapter API and more

--- actions/webhook.ts
+++ actions/webhook.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { Stellar } from "@/core/stellar";
-import { Webhook as WebhookUtils } from "@/core/webhook";
+import { WebhookDelivery } from "@/core/webhook-delivery";
 import {
   Checkout,
   Network,
@@ -224,7 +224,7 @@ export const triggerWebhooks = async (
 
   const results = await Promise.allSettled(
     subscribedWebhooks.map((webhook) =>
-      new WebhookUtils().deliverWebhook(
+      new WebhookDelivery().deliver(
         webhook,
         eventType as unknown as string,
         payload,
