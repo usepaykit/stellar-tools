69f06560e3314f27088d5fffd28bfd28d42da1d5
princeajuzie7 <princeajuzie1@gmail.com>
2026-02-04 14:41:45 +0100

    fix: image not loading on prod experemental fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    /*
+     {@experimental}
+     Bypass optimization to fix production loading (sharp/optimization can fail on some hosts)
+     {@link https://nextjs.org/docs/app/building-your-application/configuring/images#unoptimized}
+     */
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
